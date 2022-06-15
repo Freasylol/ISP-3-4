1794d684d8a110b0d297615ab34a11f3f2c95c6f
rom4chick <obereg2005.2002@gmail.com>
2022-06-15 21:44:26 +0300

    feat: add model description

--- electronic_store_server/store/models.py
+++ electronic_store_server/store/models.py
@@ -6,5 +6,26 @@ class User(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
+class Type(models.Model):
+    name = models.CharField(max_length=100)
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    name = models.CharField(max_length=100)
+    brandId = models.ForeignKey(Brand, null=True, blank=True, on_delete=models.CASCADE)
+    typeId = models.ForeignKey(Type, null=True, blank=True, on_delete=models.CASCADE)
+
+class Basket(models.Model):
+    userId = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+
+class BasketItem(models.Model):
+    basketId = models.OneToOneField(Basket, null=True, blank=True, on_delete=models.CASCADE)
+
+
+
 
 
